73ba8e7f48021a71e80e8595c1d6e0ecf01db8a0
Gerz <gerztrue@mail.ru>
2023-02-09 22:28:49 +0800

    Разметка основной страницы

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-import App from './App.vue'
+import App from './App2.vue'
 
 createApp(App).use(Antd).mount('#app');
