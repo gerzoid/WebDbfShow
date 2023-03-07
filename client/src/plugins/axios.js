d39ae2bdf053c23e21c3c115310b02ecccba7ba2
usilgtfoms <usilgtfoms@irkoms.ru>
2023-03-07 14:33:41 +0800

    fix seo

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axio = axios.create({
-    baseURL : 'http://localhost:5149',
-    //baseURL : 'http://ovz1.gerztrue-gmail-c.ndzjp.vps.myjino.ru:49293'
+    //baseURL : 'http://localhost:5149',
+    baseURL : 'http://ovz1.gerztrue-gmail-c.ndzjp.vps.myjino.ru:49293'
 })
