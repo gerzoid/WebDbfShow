8a509ef69bd8fab480e06d1ee0d2610df37a645d
usilgtfoms <usilgtfoms@irkoms.ru>
2023-07-17 17:50:27 +0800

    Add meta tag for SEO

--- client/src/plugins/router.js
+++ client/src/plugins/router.js
@@ -7,6 +7,7 @@ import ProgrammsPage from '../Pages/ProgrammsPage.vue'
 import HistoryPage from '../Pages/HistoryPage.vue'
 import FirstPage from '../Pages/FirstPage.vue'
 
+
 const routes = [
     { path: '/', component: FirstPage },
     { path: '/about', component: AboutPage },
